4d5cd5e6a94f8c3e3153ff5b172d2ad65279c2be
Diana-nyamai <dnyamai.dn@gmail.com>
2022-01-09 11:21:56 +0300

    update

--- index.js
+++ index.js
@@ -29,3 +29,24 @@ $(document).ready(function(){
         delay: anime.stagger(200)
     }, '-=1000')
 });
+
+// back to top button
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
